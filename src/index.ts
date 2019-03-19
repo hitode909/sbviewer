a6b0f15b0db9540813c961f277a6a3062003b66f
hitode909 <hitode909@gmail.com>
2019-03-19 18:32:14 +0900

    print keyword list, filename

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import * as register from "./routes";
 const app = express();
 const port = 8080;
 
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 register.register(app);
 
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
 });
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 import express = require("express");
 
 export const register = (app: express.Application) => {
-    app.get("/", (req, res) => {
-        res.render("index");
+  app.get("/", (req, res) => {
+    res.render("index", {
+      filename: ["hoge.json"],
+      keywords: ["a", "b"],
     });
+  });
 };
