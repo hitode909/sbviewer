4d2d08955f8e6b3e4f7243accb81684f3daa68fa
hitode909 <hitode909@gmail.com>
2019-03-19 18:31:35 +0900

    rm dist

--- dist/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = (app) => {
-    app.get("/", (req, res) => {
-        res.render("index");
-    });
-};
-//# sourceMappingURL=routes.js.map
